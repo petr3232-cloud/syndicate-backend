5a18bd7c2420bb5cdf16e54447c7af6e99af3ec7
petr3232-cloud <petrow3232@gmail.com>
2026-01-29 22:10:43 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-git init
-git add index.js
-git commit -m "fix: checklist save works stable"
-git branch -M main
-git remote add origin https://github.com/ТВОЙ_АККАУНТ/syndicate-backend.git
-git push -u origin main
